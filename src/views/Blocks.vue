a1babc4e1b8af2268e6fe4c1ac14e207e7fe4789
Grzegorz Bielski <pesiok@gmail.com>
2018-07-29 20:59:59 +0200

    boilerplate

---
+++ src/views/Blocks.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>halko</div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+// import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+
+@Component({})
+export default class Blocks extends Vue {}
+</script>
